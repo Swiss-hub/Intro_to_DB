a40e1081a6768e107c008390a2aff15471c5ed86
Ilechukwu Francis C <ilechukwufran6x@gmail.com>
2025-07-13 23:49:48 +0100

    A Magical Database for Your Dream Online Bookstore Adventure!

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -17,7 +17,7 @@ CREATE TABLE Books (
 
 CREATE TABLE Customers (
   customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer name VARCHAR(215) NOT NULL,
+    customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) NOT NULL,
     address TEXT
 );
